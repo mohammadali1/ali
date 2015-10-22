01edbe3ff657cf1e497de03f1af8fda7f159c3e1
Mohammadali <mohammadali1.ahmadip@ryerson.ca>
2015-10-22 17:19:46 -0400

    i changed  to fuck

---
+++ ali1/src/ali1.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : ali1.cpp
+// Author      : Ali1
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout<<"fuck";
+	return 0;
+}
